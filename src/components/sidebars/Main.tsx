ba36e71a3bbe737c095e061e3f77eda95c94d24a
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 00:46:02 +0100

    Add Avatar component to MainSidebar

--- src/components/sidebars/Main.tsx
+++ src/components/sidebars/Main.tsx
@@ -6,6 +6,7 @@ import { links } from "./links";
 import pageSize from "../../reactive/pageSize";
 import { RiDesignQuillPenLine } from "solid-icons/ri";
 import { JSX } from "solid-js/jsx-runtime";
+import Avatar from "../Avatar";
 
 const MainSidebar: Component = () => {
   return (
@@ -78,17 +79,11 @@ const MainSidebar: Component = () => {
               {/* POPUP START*/}
               <Popup
                 opener={() => (
-                  <div class="my-3 flex-it items-center flex-row p-3 rounded-3xl hover:bg-gray-800 hover:rounded-3xl transition duration-200 cursor-pointer">
-                    <div class="flex-it">
-                      <div class="w-10 h-10 overflow-visible">
-                        <img
-                          class="rounded-full"
-                          src="https://www.pinclipart.com/picdir/middle/133-1331433_free-user-avatar-icons-happy-flat-design-png.png"
-                        ></img>
-                      </div>
-                    </div>
+                  <div class="my-3 flex justify-center items-center flex-row p-3 rounded-3xl hover:bg-gray-800 hover:rounded-3xlcursor-pointer">
+                     
+                      <Avatar username={"ismailbentabett"}  />
                     <div class="flex-it xl:flex hidden flex-grow flex-row justify-between items-center">
-                      <div class="flex-it mx-3 font-bold">Filip99</div>
+                      <div class="flex-it mx-3 font-bold">ismailbentabett</div>
                       <div class="flex-it">
                         <FiMoreHorizontal />
                       </div>
