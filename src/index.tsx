6a4782181516f97d0a1bff4eb0af97025a946d55
ismailbentabett <ismailbentabett@gmail.com>
2023-12-21 01:08:55 +0100

    Add routes and screens to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
+import "./index.css";
+import App from "./App";
+import { Route, Router } from "@solidjs/router";
+import LoginScreen from "./screens/Login";
+import RegisterScreen from "./screens/Register";
+import HomeScreen from "./screens/Home";
 
-import './index.css'
-import App from './App'
+const root = document.getElementById("root");
 
-const root = document.getElementById('root')
+render(
+  () => (
+    <Router root={HomeScreen}>
+      <Route path="/" component={HomeScreen} />
+      <Route path="/login" component={LoginScreen} />
+      <Route path="/register" component={RegisterScreen} />
+    </Router>
+  ),
 
-render(() => <App />, root!)
+  root!
+);
