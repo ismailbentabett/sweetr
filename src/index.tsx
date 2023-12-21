f172db67a582a02a3fb19d8b6538e75b8d8c8618
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 00:45:43 +0100

    Add AuthProvider to Router

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,18 @@ import { Route, Router } from "@solidjs/router";
 import LoginScreen from "./screens/Login";
 import RegisterScreen from "./screens/Register";
 import HomeScreen from "./screens/Home";
+import { AuthProvider } from "./context/authContext";
 
 const root = document.getElementById("root");
 
 render(
   () => (
     <Router root={HomeScreen}>
-      <Route path="/" component={HomeScreen} />
-      <Route path="/login" component={LoginScreen} />
-      <Route path="/register" component={RegisterScreen} />
+      <AuthProvider>
+        <Route path="/" component={HomeScreen} />
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/register" component={RegisterScreen} />
+      </AuthProvider>
     </Router>
   ),
 
