d6b48ca531958e71672d7fad2f3698929825b4be
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 10:36:22 +0100

    Add Follower and Followable traits to User model

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Overtrue\LaravelFollow\Traits\Follower;
+use Overtrue\LaravelFollow\Traits\Followable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Follower, Followable;
 
     /**
      * The attributes that are mass assignable.
