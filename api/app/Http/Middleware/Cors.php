1218f2933c943126dddbfd5656c043119c464f1e
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 14:56:04 +0100

    Add CORS middleware for handling cross-origin requests

---
+++ api/app/Http/Middleware/Cors.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        $allowedOrigins = [
+            "*",
+        ];
+
+        $origin = $request->headers->get('Origin');
+
+        if (in_array($origin, $allowedOrigins)) {
+            $response = $next($request);
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+            return $response;
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+}
