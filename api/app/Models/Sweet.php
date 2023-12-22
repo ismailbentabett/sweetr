9a4b6c44b1d38624b42e97f0a11ef213d2bbd23a
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 10:44:58 +0100

    Add Markable trait to Sweet model

--- api/app/Models/Sweet.php
+++ api/app/Models/Sweet.php
@@ -4,11 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Maize\Markable\Markable;
+use Maize\Markable\Models\Like;
+use Maize\Markable\Models\Bookmark;
+
 
 class Sweet extends Model
 {
     use HasFactory;
+    use Markable;
     protected $fillable = ['content'];
+
+    protected static $marks = [
+        Like::class,
+    ];
 
     public function user()
     {
