a4a9536744394f65c11ea3c4da8e346278888743
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 09:53:24 +0100

    Refactor authentication context and add API integration

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,48 +1,94 @@
-import { JSX, createContext, createSignal, useContext } from "solid-js";
+// context/AuthContext.js
+import {
+  createSignal,
+  createEffect,
+  createContext,
+  useContext,
+  JSX,
+} from "solid-js";
+import axios from "axios";
 
-// Define the initial state of the authentication context
-type AuthState = {
-  isAuthenticated: boolean;
-  user: string | null;
-  setIsAuthenticated?: (value: boolean) => void;
-  setUser?: (value: string | null) => void;
-  loading?: boolean;
-  setLoading?: (value: boolean) => void;
-};
+const AuthContext = createContext();
+const apiUrl = import.meta.env["VITE_API_URL"];
 
-// Create the authentication context
-const AuthContext = createContext<AuthState>();
+export const AuthProvider = (
+  props: JSX.IntrinsicAttributes & { value: unknown } & {
+    children: JSX.Element;
+  }
+) => {
+  const [user, setUser] = createSignal(null);
 
-const AuthProvider = (props: {
-  children:
-    | number
-    | boolean
-    | Node
-    | JSX.ArrayElement
-    | (string & {})
-    | null
-    | undefined;
-}) => {
-  const [isAuthenticated, setIsAuthenticated] = createSignal(false);
-  const [user, setUser] = createSignal<string | null>(null);
-  const [loading, setLoading] = createSignal(true);
+  // Check if the user is authenticated on page load
+  createEffect(() => {
+    // Make a request to your Laravel backend to check authentication status
+    axios
+      .get(`${apiUrl}/user`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => setUser(response.data))
+      .catch(() => setUser(null));
+  });
+
+  const login = async (credentials: any) => {
+    try {
+      const response = await axios.post(`${apiUrl}/login`, credentials);
+
+      if (!response.data.token) {
+        throw new Error("Login failed");
+      }
+
+      localStorage.setItem("token", response.data.token);
+
+      // Make a request to get user data and update the user state
+      const userResponse = await axios.get(`${apiUrl}/user`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${response.data.token}`,
+        },
+      });
+
+      setUser(userResponse.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await axios.post(
+        `${apiUrl}/logout`,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      localStorage.removeItem("token");
+      setUser(null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const value = { user, login, logout };
 
   return (
-    <AuthContext.Provider
-      value={{
-        isAuthenticated: isAuthenticated(),
-        user: user(),
-        setIsAuthenticated,
-        setUser,
-        loading: loading(),
-        setLoading,
-      }}
-    >
-      {props.children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
   );
 };
 
-const useAuthContext = () => useContext(AuthContext);
-
-export { AuthContext, AuthProvider, useAuthContext };
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
