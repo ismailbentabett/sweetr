ece76efc0d19873b28e56f314b3d1d1e6dbbf33e
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 10:57:56 +0100

    Add comments relationship to Sweet model

--- api/app/Models/Sweet.php
+++ api/app/Models/Sweet.php
@@ -24,4 +24,9 @@ class Sweet extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
