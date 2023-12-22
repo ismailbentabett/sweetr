5ea810813b8022fd3c2c64bd9f5eb52ed058615f
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 21:31:33 +0100

    Add loading bar and axios interceptors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-// App.tsx
-import { JSX } from "solid-js";
-
+import axios from "axios";
+import { JSX, createSignal, onCleanup } from "solid-js";
+import LoadingBar, { LoadingBarRef } from "solid-top-loading-bar";
 const App = (props: {
   children?:
     | number
@@ -11,8 +11,40 @@ const App = (props: {
     | null
     | undefined;
 }) => {
+  const [loadingBar, setLoadingBar] = createSignal<LoadingBarRef>();
+
+  axios.interceptors.request.use(
+    (config) => {
+      // Start the loading bar when a request is initiated
+      loadingBar()?.continuousStart();
+      return config;
+    },
+    (error) => {
+      // Handle request errors
+      loadingBar()?.complete();
+    }
+  );
+  // Axios response interceptor
+  axios.interceptors.response.use(
+    (response) => {
+      // Stop the loading bar when a response is received
+      loadingBar()?.complete();
+      return response;
+    },
+    (error) => {
+      // Handle response errors
+      loadingBar()?.complete();
+    }
+  );
+
   return (
     <div>
+      <LoadingBar
+        color="#f11946"
+        loadingBar={loadingBar()}
+        setLoadingBar={setLoadingBar}
+      />
+
       <div id="popups" />
       {props.children}
     </div>
