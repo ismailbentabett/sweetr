267b5d764016e8aef704ba54b805b84cce3405fd
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 14:57:01 +0100

    Refactor Popup component and add logout functionality

--- src/components/utils/Popup.tsx
+++ src/components/utils/Popup.tsx
@@ -1,75 +1,94 @@
-import { Component, createEffect, createSignal, onCleanup, onMount, Show } from "solid-js";
+import {
+  Component,
+  createEffect,
+  createSignal,
+  onCleanup,
+  onMount,
+  Show,
+} from "solid-js";
 import { Portal } from "solid-js/web";
 import pageSize from "../../helpers/pageSize";
+import { useAuth } from "../../context/authContext";
 
 type Props = {
   opener: Component;
-}
+};
 
-const Popup: Component<Props> = ({opener: Opener}) => {
+const Popup: Component<Props> = ({ opener: Opener }) => {
   const [isOpen, setIsOpen] = createSignal();
+  const { logout } = useAuth();
+
+  const handleLogout = async () => {
+    await logout();
+  };
 
   let followTo: HTMLDivElement;
   let popup: HTMLDivElement;
 
   onMount(() => {
     window.addEventListener("click", closePopup);
-  })
+  });
 
   onCleanup(() => {
     window.removeEventListener("click", closePopup);
-  })
+  });
 
   createEffect(() => {
     if (isOpen() && pageSize.value()) {
       adjustPopup();
     }
-  })
+  });
 
-  const adjustPopup = () => { 
+  const adjustPopup = () => {
     if (!!popup) {
       const position = followTo.getBoundingClientRect();
       popup.style.left = position.left + "px";
       popup.style.bottom = followTo.clientHeight + "px";
     }
-  }
+  };
 
   const closePopup = (e: MouseEvent) => {
     if (isOpen() && !isPopupClicked(e)) {
       setIsOpen(false);
     }
-  }
+  };
 
   const isPopupClicked = (e: MouseEvent) => {
     return popup?.contains(e.target as Node);
-  }
+  };
 
   return (
     <div class="flex-it flex-grow">
-      <div 
+      <div
         ref={followTo!}
         onClick={(e) => {
           e.stopImmediatePropagation();
-          setIsOpen(!isOpen())
-        }
-      }>
+          setIsOpen(!isOpen());
+        }}
+      >
         <Opener />
       </div>
       <Show when={isOpen()}>
         <Portal mount={document.getElementById("popups") as Node}>
-          <div 
+          <div
             ref={popup!}
-            class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000">
+            class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000"
+          >
             <div class="w-72 min-w-68 max-h-120 min-h-8 flex-it overflow-auto">
               <div class="flex-it flex-grow flex-shrink py-3">
-                <div class="flex-it px-4 py-3 transition hover:bg-gray-700">Logout</div>
+                <button
+                  class="flex-it px-4 py-3 transition hover:bg-gray-700 h-full"
+                  onclick={handleLogout}
+                >
+                  Logout
+                </button>
               </div>
             </div>
           </div>
         </Portal>
       </Show>
     </div>
-  )
-}
+  );
+};
 
 export default Popup;
