aedb94ef1ce9a61cbe25bb891907184996c85c40
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 22:13:43 +0100

    Add Markable trait and models for Like and Bookmark

--- api/app/Models/Comment.php
+++ api/app/Models/Comment.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Maize\Markable\Markable;
+use Maize\Markable\Models\Like;
+use Maize\Markable\Models\Bookmark;
 
 class Comment extends Model
 {
     use HasFactory;
-
+    use Markable;
     protected $fillable = ['content'];
-
+    protected static $marks = [
+        Like::class,
+        Bookmark::class
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
