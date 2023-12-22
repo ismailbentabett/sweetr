44fb3370c1f1af3c36e48cf25a7f328bdc9e1590
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 10:23:48 +0100

    Refactor LoginScreen component and add login functionality

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,56 +1,88 @@
-import { A } from "@solidjs/router";
+// LoginScreen.tsx
+import { A, useNavigate } from "@solidjs/router";
 import { Component } from "solid-js";
+import useForm from "../hooks/useForm";
+import Input from "../components/form/Input";
 
 const LoginScreen: Component = () => {
-  
+  const navigate = useNavigate();
+
+  const { values, errors, isValid, handleChange, handleSubmit } = useForm({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = async () => {
+    // Handle login logic here
+    if (isValid()) {
+      try {
+        const response = await fetch("/api/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values()),
+        });
+
+        if (!response.ok) {
+          throw new Error("Login failed");
+        }
+
+        // Optionally, you can redirect the user after successful login
+        navigate("/dashboard");
+      } catch (error) {
+        console.error("Login error:", error);
+      }
+    }
+  };
+
   return (
     <div class="flex-it justify-center items-center h-screen">
-      <div class="text-white text-4xl font-bold">Sweetr - LOGIN</div>
+      <div class="text-white text-4xl font-bold">Sweetr - Login</div>
       <div class="mt-10 flex-it h-100 xs:w-100 w-full bg-gray-800 p-10 rounded-2xl">
         <div class="flex-it">
           <form class="flex-it">
             <div class="flex-it overflow-hidden sm:rounded-md">
               <div class="flex-it">
                 <div class="flex-it">
-                  <div class="flex-it py-2">
-                    <label class="block text-sm font-medium text-white">
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      class="mt-1 block w-full rounded-md text-white border-gray-500 bg-gray-800 shadow-sm focus:border-froly-500 focus:ring-froly-500 sm:text-sm"
-                    />
-                    <div class="flex-it grow text-xs bg-red-400 text-white p-3 pl-3 mt-1 rounded-md ">
-                      Error Error Beep Beep!
-                    </div>
-                  </div>
-                  <div class="flex-it py-2">
-                    <label class="block text-sm font-medium text-white">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      class="mt-1 block w-full rounded-md border-gray-500 text-white bg-gray-800 shadow-sm focus:border-froly-500 focus:ring-froly-500 sm:text-sm"
-                    />
-                  </div>
+                  <Input
+                    label="Email"
+                    name="email"
+                    type="text"
+                    value={values().email}
+                    error={errors().email}
+                    onInput={(e: { target: { value: string } }) =>
+                      handleChange("email", e.target.value)
+                    }
+                  />
+
+                  <Input
+                    label="Password"
+                    name="password"
+                    type="password"
+                    value={values().password}
+                    error={errors().password}
+                    onInput={(e: { target: { value: string } }) =>
+                      handleChange("password", e.target.value)
+                    }
+                  />
                 </div>
               </div>
               <div class="text-sm text-gray-500 pb-4">
-                No Account Yet?{" "}
+                Don't have an account?{" "}
                 <A class="underline" href="/register">
-                  Create a new account
+                  Register now
                 </A>
               </div>
               <div class="flex-it py-2">
                 <button
                   type="button"
                   class="
-                  bg-froly-400 hover:bg-froly-500
-                  inline-flex focus:ring-0 disabled:cursor-not-allowed disabled:bg-gray-400 justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white shadow-sm  focus:outline-none focus:ring-offset-2"
+                  bg-froly-400 hover:bg-froly-500 focus:ring-0
+                  disabled:cursor-not-allowed disabled:bg-gray-400
+                  inline-flex justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-offset-2"
+                  onClick={handleLogin}
+                  disabled={!isValid()}
                 >
                   Login
                 </button>
