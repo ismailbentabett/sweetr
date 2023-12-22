feb4445b523f89a94ad629e33c3b89d989eb662d
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 10:57:50 +0100

    Add Comment model

---
+++ api/app/Models/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['content'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sweet()
+    {
+        return $this->belongsTo(Sweet::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+}
