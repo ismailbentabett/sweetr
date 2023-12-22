526dc9dfd435494752f5f096c07b4fbcb927c4d1
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 10:20:45 +0100

    Add registration logic and form validation to RegisterScreen.tsx

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,9 +1,46 @@
-import { A } from "@solidjs/router";
+// RegisterScreen.tsx
+import { A, useNavigate } from "@solidjs/router";
 import { Component } from "solid-js";
+import useForm from "../hooks/useForm";
+import Input from "../components/form/Input";
 
 const RegisterScreen: Component = () => {
+  const navigate = useNavigate();
+
+  const { values, errors, isValid, handleChange, handleSubmit } = useForm({
+    fullName: "",
+    nickName: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+
+  const handleRegister = async () => {
+    // Handle registration logic here
+    if (isValid()) {
+      try {
+        const response = await fetch("/api/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values()),
+        });
+
+        if (!response.ok) {
+          throw new Error("Registration failed");
+        }
+
+        // Optionally, you can redirect the user after successful registration
+        navigate("/login");
+      } catch (error) {
+        console.error("Registration error:", error);
+      }
+    }
+  };
+
   return (
-    <div class="flex-it justify-center items-center h-screen ">
+    <div class="flex-it justify-center items-center h-screen">
       <div class="text-white text-4xl font-bold">Sweetr - Create Account</div>
       <div class="mt-10 flex-it h-100 xs:w-100 w-full bg-gray-800 p-10 rounded-2xl">
         <div class="flex-it">
@@ -11,68 +48,38 @@ const RegisterScreen: Component = () => {
             <div class="flex-it overflow-hidden sm:rounded-md">
               <div class="flex-it">
                 <div class="flex-it">
-                  <div class="flex-it py-2">
-                    <label class="block text-sm font-medium text-white">
-                      Full Name
-                    </label>
-                    <input
-                      type="text"
-                      name="fullName"
-                      id="fullName"
-                      class="mt-1 block w-full rounded-md  border-gray-500 text-white bg-gray-800  shadow-sm focus:border-froly-500 focus:ring-froly-500 sm:text-sm"
-                    />
-                    <div class="flex-it grow text-xs bg-red-400 text-white p-3 pl-3 mt-1 rounded-md">
-                      Error Error Beep Beep!
-                    </div>
-                  </div>
+                  <Input
+                    label="name"
+                    name="name"
+                    type="text"
+                    value={values().fullName}
+                    error={errors().fullName}
+                    onInput={(e: { target: { value: string } }) =>
+                      handleChange("name", e.target.value)
+                    }
+                  />
 
-                  <div class="flex-it py-2">
-                    <label class="block text-sm font-medium text-white">
-                      Nick Name
-                    </label>
-                    <input
-                      type="text"
-                      name="nickName"
-                      id="nickName"
-                      class="mt-1 block w-full rounded-md  border-gray-500 text-white bg-gray-800  shadow-sm focus:border-froly-500 focus:ring-froly-500 sm:text-sm"
-                    />
-                  </div>
+                  <Input
+                    label="Email"
+                    name="email"
+                    type="text"
+                    value={values().email}
+                    error={errors().email}
+                    onInput={(e: { target: { value: string } }) =>
+                      handleChange("email", e.target.value)
+                    }
+                  />
 
-                  <div class="flex-it py-2">
-                    <label class="block text-sm font-medium text-white">
-                      Email
-                    </label>
-                    <input
-                      type="text"
-                      name="email"
-                      id="email"
-                      class="mt-1 block w-full rounded-md  border-gray-500 text-white bg-gray-800  shadow-sm focus:border-froly-500 focus:ring-froly-500 sm:text-sm"
-                    />
-                  </div>
-
-                  <div class="flex-it py-2">
-                    <label class="block text-sm font-medium text-white">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      class="mt-1 block w-full rounded-md  border-gray-500 text-white bg-gray-800  shadow-sm focus:border-froly-500 focus:ring-froly-500 sm:text-sm"
-                    />
-                  </div>
-
-                  <div class="flex-it py-2">
-                    <label class="block text-sm font-medium text-white">
-                      Password Confirmation
-                    </label>
-                    <input
-                      type="password"
-                      name="passwordConfirmation"
-                      id="passwordConfirmation"
-                      class="mt-1 block w-full rounded-md  border-gray-500 text-white bg-gray-800  shadow-sm focus:border-froly-500 focus:ring-froly-500 sm:text-sm"
-                    />
-                  </div>
+                  <Input
+                    label="Password"
+                    name="password"
+                    type="password"
+                    value={values().password}
+                    error={errors().password}
+                    onInput={(e: { target: { value: string } }) =>
+                      handleChange("password", e.target.value)
+                    }
+                  />
                 </div>
               </div>
               <div class="text-sm text-gray-500 pb-4">
@@ -88,6 +95,8 @@ const RegisterScreen: Component = () => {
                   bg-froly-400 hover:bg-froly-500 focus:ring-0
                   disabled:cursor-not-allowed disabled:bg-gray-400
                   inline-flex justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-offset-2"
+                  onClick={handleRegister}
+                  disabled={!isValid()}
                 >
                   Register
                 </button>
