53a8bcc865ab076a107ebcb1d46448e96e864cae
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 20:13:22 +0100

    Update authClient configuration in auth.ts

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,14 @@
 import axios from "axios";
 
 export const authClient = axios.create({
-  baseURL: "http://localhost:8000/",
-  withCredentials: true, // required to handle the CSRF token
+  baseURL: "http://localhost:8000",
+  withCredentials: true,
+  xsrfCookieName: "XSRF-TOKEN",
+  xsrfHeaderName: "X-XSRF-TOKEN",
+  withXSRFToken: true,
+  
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
