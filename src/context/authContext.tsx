411e529a592827710d4c460f20ad04dbbe77037e
ismailbentabett <ismailbentabett@gmail.com>
2023-12-22 14:57:04 +0100

    Refactor authContext.tsx for improved readability and security

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,42 +1,57 @@
 // context/AuthContext.js
-import { createSignal, createEffect, createContext, useContext, JSX } from 'solid-js';
-import axios from 'axios';
+import {
+  createSignal,
+  createEffect,
+  createContext,
+  useContext,
+  JSX,
+} from "solid-js";
+import { authClient } from "../services/auth";
+import Cookies from 'js-cookie';
 
 const AuthContext = createContext();
-const baseUrl = import.meta.env.BASE_URL;
 
-export const AuthProvider = (props: JSX.IntrinsicAttributes & { value: unknown } & { children: JSX.Element }) => {
+export const AuthProvider = (
+  props: JSX.IntrinsicAttributes & { value: unknown } & {
+    children: JSX.Element;
+  }
+) => {
   const [user, setUser] = createSignal(null);
 
   // Check if the user is authenticated on page load
   createEffect(() => {
     // Make a request to your Laravel backend to check authentication status
-    axios.get(`${baseUrl}/user`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    })
-      .then((response) => setUser(response.data))
+    authClient
+      .get(`user`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response: { data: any }) => setUser(response.data))
       .catch(() => setUser(null));
   });
 
   const login = async (credentials: { email: string; password: string }) => {
     try {
-      const response = await axios.post(`${baseUrl}/login`, credentials);
+      // Fetch CSRF token
+      await authClient.get(`sanctum/csrf-cookie`);
+
+      // Make a request to login
+      const response = await authClient.post(`login`, credentials);
 
       if (!response.data.token) {
-        throw new Error('Login failed');
+        throw new Error("Login failed");
       }
 
-      localStorage.setItem('token', response.data.token);
+      localStorage.setItem("token", response.data.token);
 
       // Make a request to get user data and update the user state
-      const userResponse = await axios.get(`${baseUrl}/user`, {
+      const userResponse = await authClient.get(`user`, {
         headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
+          "Content-Type": "application/json",
+          Accept: "application/json",
           Authorization: `Bearer ${response.data.token}`,
         },
       });
@@ -49,42 +64,55 @@ export const AuthProvider = (props: JSX.IntrinsicAttributes & { value: unknown }
 
   const register = async (userData: { name: string; email: string; password: string }) => {
     try {
-      const response = await axios.post(`${baseUrl}/register`, userData);
-
-      if (!response.data.token) {
-        throw new Error('Registration failed');
-      }
-
-      localStorage.setItem('token', response.data.token);
-
+      // Fetch CSRF token
+      await authClient.get(`sanctum/csrf-cookie`);
+      //get crsf from cookies 
+      const cookies = Cookies.get('XSRF-TOKEN')
+    // Make a request to register with the CSRF token in the headers
+    const response = await authClient.post(`register`, userData, {
+      headers: {
+        'X-XSRF-TOKEN': cookies,
+      },
+    });
+  
+      localStorage.setItem("token", response.data.token);
+  
       // Make a request to get user data and update the user state
-      const userResponse = await axios.get(`${baseUrl}/user`, {
+      const userResponse = await authClient.get(`user`, {
         headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
+          "Content-Type": "application/json",
+          Accept: "application/json",
           Authorization: `Bearer ${response.data.token}`,
         },
       });
-
+  
       setUser(userResponse.data);
+      console.log("Registration successful");
     } catch (error) {
-      console.error(error);
+      console.error("Registration failed:", error);
+      // Handle different types of errors appropriately
     }
   };
+  
 
   const logout = async () => {
+    console.log(`logout`);
     try {
-      await axios.post(`${baseUrl}/logout`, {}, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
+      await authClient.post(
+        `logout`,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
 
-      localStorage.removeItem('token');
+      localStorage.removeItem("token");
       setUser(null);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 
@@ -98,7 +126,7 @@ export const AuthProvider = (props: JSX.IntrinsicAttributes & { value: unknown }
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };
