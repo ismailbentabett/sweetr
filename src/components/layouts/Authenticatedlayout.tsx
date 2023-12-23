b43cd736a5a2456fba66dd64002c6268d2016a9d
ismailbentabett <ismailbentabett@gmail.com>
2023-12-23 12:42:26 +0100

    Refactor Authenticatedlayout component and add Modal component

--- src/components/layouts/Authenticatedlayout.tsx
+++ src/components/layouts/Authenticatedlayout.tsx
@@ -5,7 +5,8 @@ import { JSX } from "solid-js/jsx-runtime";
 import { useAuth } from "../../context/authContext";
 import MainSidebar from "../sidebars/Main";
 import TrendsSidebar from "../sidebars/Trends";
-import { createDeferred, createEffect, createMemo } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
+import Modal from "../form/modal";
 
 interface AuthenticatedlayoutProps {
   children:
@@ -20,37 +21,40 @@ interface AuthenticatedlayoutProps {
 
 const Authenticatedlayout = (props: AuthenticatedlayoutProps) => {
   const navigate = useNavigate();
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
 
   createEffect(() => {
-
-    if (localStorage.getItem("isAuthenticated") === "false") {
+    if (!isAuthenticated) {
       navigate("/login");
     }
   });
 
+  const [isOpen, setIsOpen] = createSignal(false);
+
+  const handleOpenSidebar = (value: any) => {
+    setIsOpen(value);
+  };
+
   return (
-    <div class="w-full h-full bg-gray-900 text-gray-100">
-      <div class="flex h-full min-h-252">
-        <MainSidebar />
-        <main class="flex-it flex-grow flex-shrink items-start">
-          <div class="flex md:w-248 w-full h-full">
-            <div class="flex-it flex-grow flex-shrink">
-              <div class="flex-it flex-row justify-between min-h-full">
-                <div class="flex-it md:max-w-152 w-full border-x-1 border-solid border-gray-700">
-                  <div class="sticky z-10 flex-it top-0">
-                    <div class="flex-it h-14 p-4 xl:text-xl text-sm font-bold z-10 backdrop-blur-md bg-opacity-70">
+    <div class="flex h-screen bg-gray-900 text-gray-100">
+      {/* Main Content */}
+      <div class="flex flex-grow">
+        <div class="lg:w-1/4">
+          <MainSidebar openSidebar={handleOpenSidebar} />
+          <Modal isOpen={isOpen()} />
+        </div>
+
+        {/* Tweets Feed */}
+        <main class="flex-1 p-4  border-x-1 border-solid border-gray-700  xl:max-w-3xl">
+        <div class="flex-it h-14 p-4 xl:text-xl text-sm font-bold z-10 backdrop-blur-md bg-opacity-70">
                       Home
                     </div>
-                  </div>
-                  {props.children}
-                </div>
-                <div class="flex-it md:w-92 w-0 mt-4">
-                  <TrendsSidebar />
-                </div>
-              </div>
-            </div>
-          </div>
+          {props.children}
         </main>
+
+        <div class="hidden xl:block w-1/5 mx-4 mt-5">
+          <TrendsSidebar />
+        </div>
       </div>
     </div>
   );
