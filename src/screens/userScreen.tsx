384693546c57df3bd1e6f41f9d4e7abdb8b3fb09
ismailbentabett <ismailbentabett@gmail.com>
2023-12-23 15:29:21 +0100

    Redirect to profiel since the id is the same

--- src/screens/userScreen.tsx
+++ src/screens/userScreen.tsx
@@ -3,18 +3,27 @@ import Authenticatedlayout from "../components/layouts/Authenticatedlayout";
 import Avatar from "../components/Avatar";
 import SweetPost from "../components/sweets/SweetPost";
 import { useSweet } from "../context/sweetContext";
-import { useParams } from "@solidjs/router";
+import { useNavigate, useParams } from "@solidjs/router";
+import { useAuth } from "../context/authContext";
 
 const userScreen: Component = () => {
-  const { userSweets , fetchUserSweets } = useSweet();
-//get user id from url
+  //get user id from url
   const params = useParams();
   const userId = params.id;
+  const navigate = useNavigate();
+
+  const { user } = useAuth();
+  createEffect(() => {
+    if (user().id !== userId) {
+      navigate("/profile");
+    }
+  });
+
+  const { userSweets, fetchUserSweets } = useSweet();
 
   createEffect(() => {
     fetchUserSweets(userId);
   });
-
 
   return (
     <Authenticatedlayout>
@@ -29,17 +38,18 @@ const userScreen: Component = () => {
         <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
           <div class="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
             <div class="flex">
-           
-     <span  class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32">
-             <Avatar username="ismailbentabett" size={140}  />{" "}
-             </span>            </div>
+              <span class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32">
+                <Avatar username="ismailbentabett" size={140} />{" "}
+              </span>{" "}
+            </div>
             <div class="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
               <div class="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
                 <h1 class="truncate text-2xl font-bold text-white">
-                ISMAIL BENTABET
+                  ISMAIL BENTABET
                 </h1>
-                <p class="text-sm font-medium text-gray-400">@ismailbentabett</p>
-
+                <p class="text-sm font-medium text-gray-400">
+                  @ismailbentabett
+                </p>
               </div>
               <div class="justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
                 <button
@@ -58,63 +68,74 @@ const userScreen: Component = () => {
             <p class="text-sm font-medium text-gray-400">@ismailbentabett</p>
           </div>
         </div>
-        
-          <div class="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
-            <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-              <div class="sm:col-span-1">
-                <dt class="text-sm font-medium text-gray-500">Phone</dt>
-                <dd class="mt-1 text-sm text-white">(555) 123-4567</dd>
-              </div>
 
-              <div class="sm:col-span-1">
-                <dt class="text-sm font-medium text-gray-500">Email</dt>
-                <dd class="mt-1 text-sm text-white">ricardocooper@example.com</dd>
-              </div>
+        <div class="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
+          <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Phone</dt>
+              <dd class="mt-1 text-sm text-white">(555) 123-4567</dd>
+            </div>
 
-              <div class="sm:col-span-1">
-                <dt class="text-sm font-medium text-gray-500">Title</dt>
-                <dd class="mt-1 text-sm text-white">Senior Front-End Developer</dd>
-              </div>
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Email</dt>
+              <dd class="mt-1 text-sm text-white">ricardocooper@example.com</dd>
+            </div>
 
-              <div class="sm:col-span-1">
-                <dt class="text-sm font-medium text-gray-500">Team</dt>
-                <dd class="mt-1 text-sm text-white">Product Development</dd>
-              </div>
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Title</dt>
+              <dd class="mt-1 text-sm text-white">
+                Senior Front-End Developer
+              </dd>
+            </div>
 
-              <div class="sm:col-span-1">
-                <dt class="text-sm font-medium text-gray-500">Location</dt>
-                <dd class="mt-1 text-sm text-white">San Francisco</dd>
-              </div>
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Team</dt>
+              <dd class="mt-1 text-sm text-white">Product Development</dd>
+            </div>
 
-              <div class="sm:col-span-1">
-                <dt class="text-sm font-medium text-gray-500">Sits</dt>
-                <dd class="mt-1 text-sm text-white">Oasis, 4th floor</dd>
-              </div>
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Location</dt>
+              <dd class="mt-1 text-sm text-white">San Francisco</dd>
+            </div>
 
-              <div class="sm:col-span-1">
-                <dt class="text-sm font-medium text-gray-500">Salary</dt>
-                <dd class="mt-1 text-sm text-white">$145,000</dd>
-              </div>
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Sits</dt>
+              <dd class="mt-1 text-sm text-white">Oasis, 4th floor</dd>
+            </div>
 
-              <div class="sm:col-span-1">
-                <dt class="text-sm font-medium text-gray-500">Birthday</dt>
-                <dd class="mt-1 text-sm text-white">June 8, 1990</dd>
-              </div>
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Salary</dt>
+              <dd class="mt-1 text-sm text-white">$145,000</dd>
+            </div>
 
-              <div class="sm:col-span-2">
-                <dt class="text-sm font-medium text-gray-500">About</dt>
-                <dd class="mt-1 max-w-prose space-y-5 text-sm text-white">
-                  <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
-                  <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
-                </dd>
-              </div>
-            </dl>
-          </div>
-          <For each={userSweets()} fallback={<div></div>} >
-        { (sweet) =>
-          <SweetPost sweet={sweet} />
-        }
-      </For> 
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Birthday</dt>
+              <dd class="mt-1 text-sm text-white">June 8, 1990</dd>
+            </div>
+
+            <div class="sm:col-span-2">
+              <dt class="text-sm font-medium text-gray-500">About</dt>
+              <dd class="mt-1 max-w-prose space-y-5 text-sm text-white">
+                <p>
+                  Tincidunt quam neque in cursus viverra orci, dapibus nec
+                  tristique. Nullam ut sit dolor consectetur urna, dui cras nec
+                  sed. Cursus risus congue arcu aenean posuere aliquam.
+                </p>
+                <p>
+                  Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea
+                  rhoncus ac mauris amet. Urna, sem pretium sit pretium urna,
+                  senectus vitae. Scelerisque fermentum, cursus felis dui
+                  suspendisse velit pharetra. Augue et duis cursus maecenas eget
+                  quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent
+                  dictum risus suspendisse.
+                </p>
+              </dd>
+            </div>
+          </dl>
+        </div>
+        <For each={userSweets()} fallback={<div></div>}>
+          {(sweet) => <SweetPost sweet={sweet} />}
+        </For>
       </div>
     </Authenticatedlayout>
   );
