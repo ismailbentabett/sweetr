703e5bf0dd6396eb98248d831959568a6d734221
ismailbentabett <ismailbentabett@gmail.com>
2023-12-23 13:53:57 +0100

    Add data to profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -45,6 +45,58 @@ const ProfileScreen: Component = () => {
             <p class="text-sm font-medium text-gray-400">@ismailbentabett</p>
           </div>
         </div>
+        
+          <div class="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
+            <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+              <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Phone</dt>
+                <dd class="mt-1 text-sm text-white">(555) 123-4567</dd>
+              </div>
+
+              <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Email</dt>
+                <dd class="mt-1 text-sm text-white">ricardocooper@example.com</dd>
+              </div>
+
+              <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Title</dt>
+                <dd class="mt-1 text-sm text-white">Senior Front-End Developer</dd>
+              </div>
+
+              <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Team</dt>
+                <dd class="mt-1 text-sm text-white">Product Development</dd>
+              </div>
+
+              <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Location</dt>
+                <dd class="mt-1 text-sm text-white">San Francisco</dd>
+              </div>
+
+              <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Sits</dt>
+                <dd class="mt-1 text-sm text-white">Oasis, 4th floor</dd>
+              </div>
+
+              <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Salary</dt>
+                <dd class="mt-1 text-sm text-white">$145,000</dd>
+              </div>
+
+              <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Birthday</dt>
+                <dd class="mt-1 text-sm text-white">June 8, 1990</dd>
+              </div>
+
+              <div class="sm:col-span-2">
+                <dt class="text-sm font-medium text-gray-500">About</dt>
+                <dd class="mt-1 max-w-prose space-y-5 text-sm text-white">
+                  <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
+                  <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
+                </dd>
+              </div>
+            </dl>
+          </div>
       </div>
     </Authenticatedlayout>
   );
