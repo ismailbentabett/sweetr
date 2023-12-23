b9f94d86a6ddc34a61f0bccf44c43b67f3ab1d2d
ismailbentabett <ismailbentabett@gmail.com>
2023-12-23 14:20:41 +0100

    Refactor SweetPost component

--- src/components/sweets/SweetPost.tsx
+++ src/components/sweets/SweetPost.tsx
@@ -7,26 +7,26 @@ type Props = {
   sweet: Sweet;
 };
 
-const SweetPost = (props: { sweet: any }) => {
+const SweetPost = (props: { sweet: Sweet }) => {
   const sweet = () => props.sweet;
-  const user = () => sweet().user;
+  /* const user = () => sweet().user; */
 
   return (
     <div class="flex-it p-4 border-b-1 border-solid border-gray-700">
       <div class="flex-it flex-row">
         <div class="flex-it mr-4">
-          <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
+        {/*   <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
             <img class="rounded-full" src={user().avatar}></img>
-          </div>
+          </div> */}
         </div>
         <article class="flex-it flex-grow flex-shrink cursor-pointer">
           <div class="flex-it justify-center flex-grow mb-1">
             <div class="flex-it justify-between flex-row w-full">
-              <div>
+             {/*  <div>
                 <span class="font-bold">{user().nickName}</span>
                 <span class="mx-2">&#8226;</span>
                 <span class="text-gray-400">2h</span>
-              </div>
+              </div> */}
               <div class="text-gray-400 cursor-pointer transition hover:text-red-400">
                 <FiTrash size={16} />
               </div>
@@ -35,7 +35,7 @@ const SweetPost = (props: { sweet: any }) => {
           <div class="flex-it flex-row flex-grow-0 items-center mb-2">
             <div class="flex-it mr-3 mb-3 w-full">{sweet().content}</div>
           </div>
-          <div class="flex-it flex-row flex-grow text-gray-400">
+      {/*     <div class="flex-it flex-row flex-grow text-gray-400">
             <div class="flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-froly-400">
               <AiOutlineMessage size={18} />
               <span class="text-xs ml-3">{sweet().subsweetsCount}</span>
@@ -44,7 +44,7 @@ const SweetPost = (props: { sweet: any }) => {
               <FaRegularHeart size={18} />
               <span class="text-xs ml-3">{sweet().likesCount}</span>
             </div>
-          </div>
+          </div> */}
         </article>
       </div>
     </div>
