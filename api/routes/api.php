43101803c7259991459dd5aab2521336b3eff035
ismailbentabett <ismailbentabett@gmail.com>
2023-12-24 09:46:28 +0100

    Update middleware in API routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -27,7 +27,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 // routes/api.php
 
 
-Route::middleware([])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::prefix('sweets')->group(function () {
         Route::get('/', [SweetController::class, 'index']);
         Route::get('/my', [SweetController::class, 'mySweets']);
