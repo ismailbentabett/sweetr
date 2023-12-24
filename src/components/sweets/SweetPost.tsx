10ec950ba456899292513f4f43cd195f517736e6
ismailbentabett <ismailbentabett@gmail.com>
2023-12-24 01:11:40 +0100

    Add like and bookmark functionality to SweetPost component

--- src/components/sweets/SweetPost.tsx
+++ src/components/sweets/SweetPost.tsx
@@ -5,54 +5,87 @@ import { Sweet } from "../../types/Sweet";
 import Avatar from "../Avatar";
 import { useAuth } from "../../context/authContext";
 import { Show } from "solid-js";
+import { useSweet } from "../../context/sweetContext";
+import { BsBookmark } from "solid-icons/bs";
 
 type Props = {
   sweet: Sweet;
 };
 
 const SweetPost = (props: { sweet: Sweet }) => {
-const {user} = useAuth() as any;
+  const { user } = useAuth() as any;
+  const { likeSweet, unlikeSweet, unbookmarkSweet } = useSweet() as any;
+
   const sweet = () => props.sweet;
   /* const user = () => sweet().user; */
 
+  const handleLike = () => {
+    likeSweet(sweet().id);
+  };
+
+  /*  const handleUnlike = () => {
+    unlikeSweet(sweet().id);
+  }
+
+  const handleBookmark = () => {
+    bookmarkSweet(sweet().id);
+  }
+
+  const handleUnbookmark = () => {
+    unbookmarkSweet(sweet().id);
+  } */
+
   return (
     <Show when={user() !== null} fallback={<div>Loading...</div>}>
-    <div class="flex-it p-4 border-b-1 border-solid border-gray-700">
-      <div class="flex-it flex-row">
-        <div class="flex-it mr-4">
-     <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
-            <Avatar username="ismailbentabett" size={140}  />{" "}
-          </div> 
-        </div>
-        <article class="flex-it flex-grow flex-shrink cursor-pointer">
-          <div class="flex-it justify-center flex-grow mb-1">
-            <div class="flex-it justify-between flex-row w-full">
-             <div>
-                <span class="font-bold">{user().name}</span>
-                <span class="mx-2">&#8226;</span>
-                <span class="text-gray-400">2h</span>
-              </div> 
-              <div class="text-gray-400 cursor-pointer transition hover:text-red-400">
-                <FiTrash size={16} />
+      <div class="flex-it p-4 border-b-1 border-solid border-gray-700">
+        <div class="flex-it flex-row">
+          <div class="flex-it mr-4">
+            <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
+              <Avatar username="ismailbentabett" size={140} />{" "}
+            </div>
+          </div>
+          <article class="flex-it flex-grow flex-shrink cursor-pointer">
+            <div class="flex-it justify-center flex-grow mb-1">
+              <div class="flex-it justify-between flex-row w-full">
+                <div>
+                  <span class="font-bold">{user().name}</span>
+                  <span class="mx-2">&#8226;</span>
+                  <span class="text-gray-400">2h</span>
+                </div>
+                <div class="text-gray-400 cursor-pointer transition hover:text-red-400">
+                  <FiTrash size={16} />
+                </div>
               </div>
             </div>
-          </div>
-          <div class="flex-it flex-row flex-grow-0 items-center mb-2">
-            <div class="flex-it mr-3 mb-3 w-full">{sweet().content}</div>
-          </div>
-           <div class="flex-it flex-row flex-grow text-gray-400">
-            <div class="flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-froly-400">
-              <AiOutlineMessage size={18} />
-              <span class="text-xs ml-3">10</span>
+            <div class="flex-it flex-row flex-grow-0 items-center mb-2">
+              <div class="flex-it mr-3 mb-3 w-full">{sweet().content}</div>
             </div>
-            <div class="flex-it flex-row items-center cursor-pointer transition hover:text-pink-400">
-              <FaRegularHeart size={18} />
-              <span class="text-xs ml-3">10</span>
+            <div class="flex-it flex-row flex-grow text-gray-400">
+              <div class="w-full flex">
+
+             
+              <div class="flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-froly-400">
+                <AiOutlineMessage size={18} />
+                <span class="text-xs ml-3">10</span>
+              </div>
+              <button
+                onClick={handleLike}
+                class="flex-it flex-row items-center cursor-pointer transition hover:text-froly-400"
+              >
+                <FaRegularHeart size={18} />
+                <span class="text-xs ml-3">10</span>
+              </button>
+              </div>
+              <button
+                onClick={handleLike}
+                class="flex-it flex-row items-center cursor-pointer transition hover:text-froly-400"
+              >
+                <BsBookmark size={15} />
+              </button>
             </div>
-          </div>
-        </article>
+          </article>
+        </div>
       </div>
-    </div>
     </Show>
   );
 };
