e1f364c3445477729cd88607777639d523d99af1
ismailbentabett <ismailbentabett@gmail.com>
2023-12-25 00:21:39 +0100

    Add BookMarks screen component

---
+++ src/screens/BookMarks.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const BookMarks: Component = () => {
+    return (
+        <div>
+            <h1>BookMarks</h1>
+        </div>
+    );
+};
+
+export default BookMarks;
