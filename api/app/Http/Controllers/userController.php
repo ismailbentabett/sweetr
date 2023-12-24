a4996d649f9ed2db638d4239ac87dbff161f56bd
ismailbentabett <ismailbentabett@gmail.com>
2023-12-24 13:17:29 +0100

    Add follow/unfollow functionality to userController.php

--- api/app/Http/Controllers/userController.php
+++ api/app/Http/Controllers/userController.php
@@ -22,4 +22,43 @@ class userController extends Controller
             'user' => $user,
         ]);
     }
+
+    //is folloing me or am i folling him
+    public function isFollowing($id)
+    {
+        $user = User::find($id);
+        $authUser = User::find(auth()->user()->id);
+
+
+        $isFollowint = $authUser->isFollowing($user);
+
+        return response()->json([
+            'isFollowing' => $isFollowint,
+        ]);
+    }
+
+    public function isFollower($id)
+    {
+        $user = User::find($id);
+        $authUser = User::find(auth()->user()->id);
+
+
+        $isFollower = $authUser->isFollower($user);
+
+        return response()->json([
+            'isFollower' => $isFollower,
+        ]);
+    }
+
+    public function isFollowedBy($followeeId, $followerId)
+    {
+        $followee = User::find($followeeId);
+        $follower = User::find($followerId);
+
+        $isFollowedBy = $followee->isFollowedBy($follower);
+
+        return response()->json([
+            'isFollowedBy' => $isFollowedBy,
+        ]);
+    }
 }
