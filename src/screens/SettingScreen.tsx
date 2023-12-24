bee81ef29ce60e737b1261e8592f995ef522fe53
ismailbentabett <ismailbentabett@gmail.com>
2023-12-25 00:21:42 +0100

    Add SettingScreen component

---
+++ src/screens/SettingScreen.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const SettingScreen: Component = () => {
+  return (
+    <div>
+      <h1>SettingScreen</h1>
+    </div>
+  );
+};
+
+export default SettingScreen;
