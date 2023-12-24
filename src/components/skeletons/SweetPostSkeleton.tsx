3d24286d323922109f255252d344d6477e768d09
ismailbentabett <ismailbentabett@gmail.com>
2023-12-24 14:17:52 +0100

    Add SweetPostSkeleton component

---
+++ src/components/skeletons/SweetPostSkeleton.tsx
@@ -0,0 +1,17 @@
+const SweetPostSkeleton = () => {
+  // Define your component logic here
+
+  return (
+    <div role="status" class="w-full animate-pulse">
+      <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+      <span class="sr-only">Loading...</span>
+    </div>
+  );
+};
+
+export default SweetPostSkeleton;
