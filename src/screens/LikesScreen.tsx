5adda5526362a8b213d11df696b3a87fead2eeec
ismailbentabett <ismailbentabett@gmail.com>
2023-12-25 00:21:35 +0100

    Add LikesScreen component

---
+++ src/screens/LikesScreen.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const LikesScreen: Component = () => {
+    return (
+        <div>
+            <h1>LikesScreen</h1>
+        </div>
+    );
+};
+
+export default LikesScreen;
