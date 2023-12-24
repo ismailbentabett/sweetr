0734b773d2f5a498b37b43395490836bc0bbfcc6
ismailbentabett <ismailbentabett@gmail.com>
2023-12-24 09:46:31 +0100

    Update SweetPost component imports and add bookmark functionality

--- src/components/sweets/SweetPost.tsx
+++ src/components/sweets/SweetPost.tsx
@@ -1,4 +1,4 @@
-import { AiOutlineMessage } from "solid-icons/ai";
+import { AiFillHeart, AiOutlineMessage } from "solid-icons/ai";
 import { FaRegularHeart } from "solid-icons/fa";
 import { FiTrash } from "solid-icons/fi";
 import { Sweet } from "../../types/Sweet";
@@ -6,7 +6,7 @@ import Avatar from "../Avatar";
 import { useAuth } from "../../context/authContext";
 import { Show } from "solid-js";
 import { useSweet } from "../../context/sweetContext";
-import { BsBookmark } from "solid-icons/bs";
+import { BsBookmark, BsBookmarkFill } from "solid-icons/bs";
 
 type Props = {
   sweet: Sweet;
@@ -14,7 +14,8 @@ type Props = {
 
 const SweetPost = (props: { sweet: Sweet }) => {
   const { user } = useAuth() as any;
-  const { likeSweet, unlikeSweet, unbookmarkSweet } = useSweet() as any;
+  const { likeSweet, unlikeSweet, unbookmarkSweet, bookmarkSweet } =
+    useSweet() as any;
 
   const sweet = () => props.sweet;
   /* const user = () => sweet().user; */
@@ -22,18 +23,17 @@ const SweetPost = (props: { sweet: Sweet }) => {
   const handleLike = () => {
     likeSweet(sweet().id);
   };
-
-  /*  const handleUnlike = () => {
+  const handleUnlike = () => {
     unlikeSweet(sweet().id);
-  }
+  };
 
   const handleBookmark = () => {
     bookmarkSweet(sweet().id);
-  }
+  };
 
   const handleUnbookmark = () => {
     unbookmarkSweet(sweet().id);
-  } */
+  };
 
   return (
     <Show when={user() !== null} fallback={<div>Loading...</div>}>
@@ -41,46 +41,54 @@ const SweetPost = (props: { sweet: Sweet }) => {
         <div class="flex-it flex-row">
           <div class="flex-it mr-4">
             <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
-              <Avatar username="ismailbentabett" size={140} />{" "}
+              <Avatar username={sweet().user!.name} size={140} />{" "}
             </div>
           </div>
           <article class="flex-it flex-grow flex-shrink cursor-pointer">
             <div class="flex-it justify-center flex-grow mb-1">
               <div class="flex-it justify-between flex-row w-full">
                 <div>
-                  <span class="font-bold">{user().name}</span>
+                  <span class="font-bold">{sweet().user!.name}</span>
                   <span class="mx-2">&#8226;</span>
                   <span class="text-gray-400">2h</span>
                 </div>
+                <Show when={sweet().user!.id === user().id}>
                 <div class="text-gray-400 cursor-pointer transition hover:text-red-400">
                   <FiTrash size={16} />
                 </div>
+                </Show>
               </div>
             </div>
             <div class="flex-it flex-row flex-grow-0 items-center mb-2">
-              <div class="flex-it mr-3 mb-3 w-full">{sweet().content}</div>
+              <div class="flex-it mr-3 mb-3 w-full">
+                {sweet().content} 
+              </div>
             </div>
             <div class="flex-it flex-row flex-grow text-gray-400">
               <div class="w-full flex">
-
-             
-              <div class="flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-froly-400">
-                <AiOutlineMessage size={18} />
-                <span class="text-xs ml-3">10</span>
+                <div class="flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-froly-400">
+                  <AiOutlineMessage size={18} />
+                  <span class="text-xs ml-3">10</span>
+                </div>
+                <button
+                  onClick={sweet().liked ? handleUnlike : handleLike}
+                  class={`flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-froly-400 ${
+                    sweet().liked ? "text-froly-400" : ""
+                  }`}
+                >
+                  {!sweet().liked && <FaRegularHeart size={18} />}
+                  {sweet().liked && <AiFillHeart size={18} color="#D91346" />}
+                  <span class="text-xs ml-3">10</span>
+                </button>
               </div>
               <button
-                onClick={handleLike}
-                class="flex-it flex-row items-center cursor-pointer transition hover:text-froly-400"
+                onClick={sweet().bookmarked ? handleUnbookmark : handleBookmark}
+                class={`flex-it flex-row items-center cursor-pointer mr-5 transition hover:text-froly-400 ${
+                  sweet().bookmarked ? "text-froly-400" : ""
+                }`}
               >
-                <FaRegularHeart size={18} />
-                <span class="text-xs ml-3">10</span>
-              </button>
-              </div>
-              <button
-                onClick={handleLike}
-                class="flex-it flex-row items-center cursor-pointer transition hover:text-froly-400"
-              >
-                <BsBookmark size={15} />
+                {!sweet().bookmarked && <BsBookmark size={18} />}
+                {sweet().bookmarked && <BsBookmarkFill size={18} color="#D91346" />}
               </button>
             </div>
           </article>
