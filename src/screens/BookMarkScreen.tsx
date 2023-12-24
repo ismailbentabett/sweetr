4e3397f59d8eb7f4e6948c98aa622e2b1d683ee7
ismailbentabett <ismailbentabett@gmail.com>
2023-12-25 00:25:12 +0100

    Add BookMarkScreen component

---
+++ src/screens/BookMarkScreen.tsx
@@ -0,0 +1,13 @@
+import { Component } from "solid-js";
+import Authenticatedlayout from "../components/layouts/Authenticatedlayout";
+
+const BookMarkScreen: Component = () => {
+    return (
+        <Authenticatedlayout>
+
+            <h1>BookMarks</h1>
+        </Authenticatedlayout>
+    );
+};
+
+export default BookMarkScreen;
