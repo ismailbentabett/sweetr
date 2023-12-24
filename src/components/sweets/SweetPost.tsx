2697cc2b45bbbe7008c7f8de9ca420f05979ad67
ismailbentabett <ismailbentabett@gmail.com>
2023-12-24 23:52:01 +0100

    Add deleteSweet function and handleDelete button in SweetPost component

--- src/components/sweets/SweetPost.tsx
+++ src/components/sweets/SweetPost.tsx
@@ -15,8 +15,13 @@ type Props = {
 
 const SweetPost = (props: { sweet: Sweet }) => {
   const { user } = useAuth() as any;
-  const { likeSweet, unlikeSweet, unbookmarkSweet, bookmarkSweet } =
-    useSweet() as any;
+  const {
+    likeSweet,
+    unlikeSweet,
+    unbookmarkSweet,
+    bookmarkSweet,
+    deleteSweet,
+  } = useSweet() as any;
 
   const sweet = () => props.sweet;
   /* const user = () => sweet().user; */
@@ -36,8 +41,19 @@ const SweetPost = (props: { sweet: Sweet }) => {
     unbookmarkSweet(sweet().id);
   };
 
+  const handleDelete = () => {
+    deleteSweet(sweet().id);
+  };
+
   return (
-    <Show when={user() !== null}fallback={<div><SweetPostSkeleton/></div>}>
+    <Show
+      when={user() !== null}
+      fallback={
+        <div>
+          <SweetPostSkeleton />
+        </div>
+      }
+    >
       <div class="flex-it p-4 border-b-1 border-solid border-gray-700">
         <div class="flex-it flex-row">
           <div class="flex-it mr-4">
@@ -54,16 +70,17 @@ const SweetPost = (props: { sweet: Sweet }) => {
                   <span class="text-gray-400">2h</span>
                 </div>
                 <Show when={sweet().user!.id === user().data.id}>
-                <div class="text-gray-400 cursor-pointer transition hover:text-red-400">
-                  <FiTrash size={16} />
-                </div>
+                  <button
+                    onClick={handleDelete}
+                    class="text-gray-400 cursor-pointer transition hover:text-red-400"
+                  >
+                    <FiTrash size={16} />
+                  </button>
                 </Show>
               </div>
             </div>
             <div class="flex-it flex-row flex-grow-0 items-center mb-2">
-              <div class="flex-it mr-3 mb-3 w-full">
-                {sweet().content} 
-              </div>
+              <div class="flex-it mr-3 mb-3 w-full">{sweet().content}</div>
             </div>
             <div class="flex-it flex-row flex-grow text-gray-400">
               <div class="w-full flex">
@@ -89,7 +106,9 @@ const SweetPost = (props: { sweet: Sweet }) => {
                 }`}
               >
                 {!sweet().bookmarked && <BsBookmark size={18} />}
-                {sweet().bookmarked && <BsBookmarkFill size={18} color="#D91346" />}
+                {sweet().bookmarked && (
+                  <BsBookmarkFill size={18} color="#D91346" />
+                )}
               </button>
             </div>
           </article>
