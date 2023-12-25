30193786d7430c2f925b3d4c29d49e79c7d247df
ismailbentabett <ismailbentabett@gmail.com>
2023-12-25 01:25:31 +0100

    Add routes for liked and bookmarked sweets

--- api/routes/api.php
+++ api/routes/api.php
@@ -67,5 +67,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/isFollowing/{id}', [userController::class, 'isFollowing']);
         Route::get('/isFollower/{id}', [userController::class, 'isFollower']);
         Route::get('/isFollowedBy/{followeeId}/{followerId}', [userController::class, 'isFollowedBy']);
+        Route::get('/likes', [UserController::class, 'likedSweets']);
+        Route::get('/bookmarks', [UserController::class, 'bookmarkedSweets']);
     });
 });
