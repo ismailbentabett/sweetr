5652b92587de32aaf4ed1c07202e5cfad35e7825
ismailbentabett <ismailbentabett@gmail.com>
2023-12-25 01:55:37 +0100

    Add FortifyServiceProvider to app.php

--- api/config/app.php
+++ api/config/app.php
@@ -171,6 +171,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
+        /* UpdatesUserProfileInformation */
     ])->toArray(),
 
     /*
