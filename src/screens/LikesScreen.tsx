bd0d6128b00f54bedcdd1bc054d8c65a564fd0a7
ismailbentabett <ismailbentabett@gmail.com>
2023-12-25 01:25:40 +0100

    Add fetchLikes function and display likes in LikesScreen

--- src/screens/LikesScreen.tsx
+++ src/screens/LikesScreen.tsx
@@ -1,12 +1,30 @@
-import { Component } from "solid-js";
+import { Component, For, Show, createEffect } from "solid-js";
 import Authenticatedlayout from "../components/layouts/Authenticatedlayout";
+import { useUser } from "../context/userContext";
+import SweetPost from "../components/sweets/SweetPost";
+import Empty from "../components/Empty";
 
 const LikesScreen: Component = () => {
-    return (
-        <Authenticatedlayout>
-            <h1>LikesScreen</h1>
-        </Authenticatedlayout>
-    );
+  const { likes, fetchLikes } = useUser() as any;
+
+    fetchLikes();
+;
+
+  return (
+    <Authenticatedlayout>
+        {JSON.stringify(likes())}
+    {/*   <Show
+        when={likes().data.length > 0}
+        fallback={
+          <div>
+            <Empty />
+          </div>
+        }
+      >
+        <For each={likes().data}>{(sweet) => <SweetPost sweet={sweet} />}</For>
+      </Show> */}
+    </Authenticatedlayout>
+  );
 };
 
 export default LikesScreen;
