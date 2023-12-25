258183692eff225f0d16c09fa846d1e144a31824
ismailbentabett <ismailbentabett@gmail.com>
2023-12-25 14:21:56 +0100

    Refactor form/modal component

--- src/components/form/modal.tsx
+++ src/components/form/modal.tsx
@@ -1,6 +1,6 @@
 import { FaRegularImage } from "solid-icons/fa";
 import type { JSX } from "solid-js";
-import { createEffect, createSignal, createUniqueId } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 import {
   Dialog,
   DialogOverlay,
@@ -11,24 +11,23 @@ import {
 } from "terracotta";
 import Avatar from "../Avatar";
 
+import { Show } from "solid-js";
+import { useSweet } from "../../context/sweetContext";
+import useForm from "../../hooks/useForm";
+import { useAuth } from "../../context/authContext";
+
 export default function Modal(props: { isOpen: any }): JSX.Element {
   const [isOpen, setIsOpen] = createSignal(props.isOpen);
-  const [content, setContent] = createSignal("");
-  const createSweet = () => {
-    const sweet = {
-      id: createUniqueId(),
-      content: content(),
-      user: {
-        nickName: "ismailbentabett",
-        avatar:
-          "https://www.pinclipart.com/picdir/middle/133-1331433_free-user-avatar-icons-happy-flat-design-png.png",
-      },
-      likesCount: 0,
-      subsweetsCount: 0,
-      date: new Date(),
-    };
+  const { user } = useAuth() as any;
+  const { sweets, createSweet, fetchSweets } = useSweet() as any;
+  const { values, errors, isValid, handleChange, handleSubmit } = useForm({
+    content: "",
+  });
 
-    setContent("");
+  const handleCreateSweet = () => {
+    createSweet({ content: values().content, user_id: user().id });
+    //empty the textarea
+    handleChange("content", "");
   };
 
   createEffect(() => {
@@ -76,59 +75,69 @@ export default function Modal(props: { isOpen: any }): JSX.Element {
                   as="h3"
                   class="text-lg font-medium leading-6 text-white"
                 >
-                 Sweet Now 
+                  Sweet Now
                 </DialogTitle>
                 <div class="flex-it py-1 px-4 flex-row">
-                  <div class="flex-it mr-4">
-                    <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
-                      <Avatar username={"ismailbentabett"} />
-                    </div>
-                  </div>
-                  {/* MESSENGER START */}
-                  <div class="flex-it flex-grow">
-                    <div class="flex-it">
-                      <textarea
-                        value={content()}
-                        onInput={(event) => {
-                          setContent(event.currentTarget.value);
-                        }}
-                        name="content"
-                        rows="1"
-                        id="sweet"
-                        class="bg-transparent resize-none overflow-hidden block !outline-none !border-none border-transparent focus:border-transparent focus:ring-0 text-gray-100 text-xl w-full p-0"
-                        placeholder={"What's new?"}
-                      />
-                    </div>
-                    <div class="flex-it mb-1 flex-row xs:justify-between items-center">
-                      <div class="flex-it mt-3 mr-3 cursor-pointer text-white hover:text-froly-400 transition">
-                        <div class="upload-btn-wrapper cursor-pointer">
-                          <FaRegularImage
-                            size={18}
-                            style={{ cursor: "pointer" }}
-                          />
-                          <input
-                            style={{ cursor: "pointer" }}
-                            type="file"
-                            name="myfile"
+                 
+                  {/* HOME PAGE START */}
+                  <div class="flex-it py-1  flex-row  w-full px-20">
+                    <div class="flex-it mr-4">
+                      <Show when={user()}>
+                        <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
+                          <Avatar
+                            username={user().data.name}
+                            size={140}
+                            userId={user().data.id}
                           />
                         </div>
+                      </Show>
+                    </div>
+                    {/* MESSENGER START */}
+                    <div class="flex-it flex-grow w-full">
+                      <div class="flex-it">
+                        <textarea
+                          value={values().content}
+                          onInput={(e: { target: { value: any } }) =>
+                            handleChange("content", e.target.value)
+                          }
+                          name="content"
+                          rows="1"
+                          id="sweet"
+                          class="bg-transparent resize-none overflow-hidden block !outline-none !border-none border-transparent focus:border-transparent focus:ring-0 text-gray-100 text-xl w-full p-0"
+                          placeholder={"What's new?"}
+                        />
                       </div>
-                      <div class="flex-it w-32 mt-3 cursor-pointer">
-                        <button
-                          onClick={createSweet}
-                          type="button"
-                          class="
+                      <div class="flex-it mb-1 flex-row xs:justify-between items-center w-full">
+                        <div class="flex-it mt-3 mr-3 cursor-pointer text-white hover:text-froly-400 transition">
+                          <div class="upload-btn-wrapper cursor-pointer">
+                            <FaRegularImage
+                              size={18}
+                              style={{ cursor: "pointer" }}
+                            />
+                            <input
+                              style={{ cursor: "pointer" }}
+                              type="file"
+                              name="myfile"
+                            />
+                          </div>
+                        </div>
+                        <div class="flex-it  mt-3 cursor-pointer">
+                          <button
+                            onClick={handleCreateSweet}
+                            type="button"
+                            class="
                   disabled:cursor-not-allowed disabled:bg-gray-400
                   bg-froly-400 hover:bg-froly-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
-                        >
-                          <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
-                            <span>Sweet It</span>
-                          </div>
-                        </button>
+                          >
+                            <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
+                              <span>Sweet It</span>
+                            </div>
+                          </button>
+                        </div>
                       </div>
                     </div>
+                    {/* MESSENGER END */}
                   </div>
-                  {/* MESSENGER END */}
                 </div>
               </DialogPanel>
             </TransitionChild>
